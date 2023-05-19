0d2c09b245702c49aa3039c94b309e1d586e796e
OliPohl <oliver@pohl66.de>
2023-05-19 21:51:00 +0200

    fixed overwriting error and qol changes

--- Code/DoorRasp/databaseHandler.py
+++ Code/DoorRasp/databaseHandler.py
@@ -1,5 +1,5 @@
 class DatabaseHandler:
-    def __init__(self, roomId, loudSeats, quietSeats):
+    def __init__(self, roomId: str, loudSeats: int, quietSeats: int):
         self.roomId = roomId
         self.roomProperties = {"isActive" : True, "loudSeats" : loudSeats, "quietSeats": quietSeats}
         
@@ -8,14 +8,14 @@ class DatabaseHandler:
         # Check if Romm exists in database
         try:
             self.databaseContent = eval(self.database.read())
-            if self.databaseContent[self.roomId]['isActive']:
+            if self.databaseContent.get(self.roomId, {}).get('isActive'):
                 print("### Exception: {roomId} is currently active. Room data is being overwritten to ensure consistency. If you encounter any issues, please verify that there are no other rooms logged in with the same ID.".format(roomId=self.roomId))
         except:
             self.databaseContent = {}
         
         
         # Logging into room
-        self.databaseContent[self.roomId] = self.roomProperties
+        self.databaseContent.setdefault(self.roomId, {}).update(self.roomProperties)
         self.database.seek(0)
         self.database.write(str(self.databaseContent))
         self.database.truncate()
@@ -25,10 +25,14 @@ class DatabaseHandler:
         self.database.seek(0)
         self.databaseContent = eval(self.database.read())
         
-        for index in self.databaseContent[self.roomId]:
-            if self.databaseContent[self.roomId][index] != self.roomProperties[index]:
-                raise Exception("Room data mismatch for {roomId}. Please ensure that the RaspberryPi is connected to the database and there are no conflicting room properties.".format(roomId=self.roomId))
-            
+        if self.roomId in self.databaseContent:
+            roomData = self.databaseContent[self.roomId]
+            for entry in self.roomProperties:
+                if roomData.get(entry) != self.roomProperties[entry]:
+                    raise Exception("Room data mismatch for {roomId}. Please ensure that the RaspberryPi is connected to the database and there are no conflicting room properties.".format(roomId=self.roomId))
+        else:
+            raise Exception("Room with ID {roomId} doesn't exist in the database. Please ensure that the RaspberryPi is connected to the database.".format(roomId=self.roomId))
+        
         print("Successfully logged into Database:\n---\nRoom ID: {roomId}\nLoud Seats: {loudSeats}\nQuiet Seats: {quietSeats}\n---".format(roomId=self.roomId, loudSeats=self.databaseContent[self.roomId]["loudSeats"], quietSeats=self.databaseContent[self.roomId]["quietSeats"]))
 
 
