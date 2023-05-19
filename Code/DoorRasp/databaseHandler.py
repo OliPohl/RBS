bc48c6d0435ce98d082edb2f564a526955f4c98b
OliPohl <oliver@pohl66.de>
2023-05-19 21:34:35 +0200

    login works now

--- Code/DoorRasp/databaseHandler.py
+++ Code/DoorRasp/databaseHandler.py
@@ -1,32 +1,33 @@
 class DatabaseHandler:
     def __init__(self, roomId, loudSeats, quietSeats):
-        # Login to the Database
         self.roomId = roomId
         self.roomProperties = {"isActive" : True, "loudSeats" : loudSeats, "quietSeats": quietSeats}
         
-        database = open("localDatabase.txt", "r+")
+        self.database = open("localDatabase.txt", "r+")
         try:
-            databaseContent = eval(database.read())
-            if databaseContent[self.roomId]['isActive']:
-                print("### Exception: {roomId} is currently active. Overwriting room data. If any errors occur, make sure there is no other room logged in with the same ID.".format(roomId=self.roomId))
+            self.databaseContent = eval(self.database.read())
+            if self.databaseContent[self.roomId]['isActive']:
+                print("### Exception: {roomId} is currently active. Room data is being overwritten to ensure consistency. If you encounter any issues, please verify that there are no other rooms logged in with the same ID.".format(roomId=self.roomId))
         except:
-            databaseContent = {}
+            self.databaseContent = {}
+        
         
         # Logging into room
-        databaseContent[self.roomId] = self.roomProperties
-        database.seek(0)
-        database.write(str(databaseContent))
-        database.truncate() 
+        self.databaseContent[self.roomId] = self.roomProperties
+        self.database.seek(0)
+        self.database.write(str(self.databaseContent))
+        self.database.truncate()
         
         
         # Check if room is now logged in and max seats are changed
-        database.seek(0)
-        databaseContent = eval(database.read())
+        self.database.seek(0)
+        self.databaseContent = eval(self.database.read())
         
-        if self.roomId not in databaseContent:
-            raise Exception("Unable to log into {roomId}. Make sure the RaspberryPi has a connection to the database.".format(roomId=self.roomId))
-        
-        print("Successfully logged into Database:\n---\nRoom ID: {roomId}\nLoud Seats: {loudSeats}\nQuiet Seats: {quietSeats}\n---".format(roomId=self.roomId, loudSeats=loudSeats, quietSeats=quietSeats))
+        for index in self.databaseContent[self.roomId]:
+            if self.databaseContent[self.roomId][index] != self.roomProperties[index]:
+                raise Exception("Room data mismatch for {roomId}. Please ensure that the RaspberryPi is connected to the database and there are no conflicting room properties.".format(roomId=self.roomId))
+            
+        print("Successfully logged into Database:\n---\nRoom ID: {roomId}\nLoud Seats: {loudSeats}\nQuiet Seats: {quietSeats}\n---".format(roomId=self.roomId, loudSeats=self.databaseContent[self.roomId]["loudSeats"], quietSeats=self.databaseContent[self.roomId]["quietSeats"]))
 
 
 
