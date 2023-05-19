05697484a63214997a8fa6ae7c5b8fb988be8fb6
OliPohl <oliver@pohl66.de>
2023-05-19 20:59:44 +0200

    added a database

--- Code/DoorRasp/databaseHandler.py
+++ Code/DoorRasp/databaseHandler.py
@@ -1,28 +1,28 @@
-import os
-import numpy as np
-
-
-class DatabaseHandler():
+class DatabaseHandler:
     def __init__(self, roomId, loudMaxSeats, quietMaxSeats):
         # Login to the Database
         self.roomId = roomId
-        self.loudMaxSeats = loudMaxSeats
-        self.quietMaxSeats = quietMaxSeats
+        self.roomProperties = {"isActive" : True, "loudMaxSeats" : loudMaxSeats, "quietMaxSeats": quietMaxSeats}
+        
+        database = open("localDatabase.txt", "r+")
+        databaseContent = database.read()
         
         # Check if room is currently logged in
-        if(False):
-            print("### Exception: {roomId} is currently active. Overwriting room data. If any errors occour make sure there is no other room logged in with the same ID.".format(roomId=self.roomId))
-            
-        # log into room
+        if self.roomId in databaseContent:
+            print("### Exception: {roomId} is currently active. Overwriting room data. If any errors occur, make sure there is no other room logged in with the same ID.".format(roomId=self.roomId))
+
+
+        # Log into the room by writing roomId
+        database.write(f"{self.roomId} = {self.roomProperties}\n")
         
+        # read database to check if successfull
+        databaseContent = database.read()
         
         # Check if room is now logged in and max seats are changed
-        if(False):
+        if self.roomId in databaseContent:
             raise Exception("Unable to log into {roomId}. Make sure the RaspberryPi has a connection to the database.".format(roomId=self.roomId))
         
-        return print("Sucessfully logged into Database: \n --- \n Room ID: {roomId} \n Maximum seats loud: {loudMaxSeats} \n Maximum seats quiet: {quietMaxSeats} \n --- ".format(roomId=self.roomId, loudMaxSeats=self.loudMaxSeats, quietMaxSeats=self.quietMaxSeats))
-    
-    
+        print("Successfully logged into Database:\n---\nRoom ID: {roomId}\nMaximum seats loud: {loudMaxSeats}\nMaximum seats quiet: {quietMaxSeats}\n---".format(roomId=self.roomId, loudMaxSeats=self.loudMaxSeats, quietMaxSeats=self.quietMaxSeats))
 
 
 
