31fa604fc472c5a194f39e689f6a9aefa67def40
OliPohl <oliver@pohl66.de>
2023-07-04 14:31:10 +0200

    Revert "time"
    This reverts commit ebec3dfca6d8fbdd3506f5f8ef7dd4e7f87252a2.

--- Code/DoorRasp/gui.py
+++ Code/DoorRasp/gui.py
@@ -285,8 +285,7 @@ class Win(tk.Tk):
         exitTimes = self.databaseHandler.GetExitTimes()
         for i in range(10):
             if i < len(exitTimes):    
-                print(exitTimes[i] + " " + datetime.now() + timedelta(hours=2))
-                timeDelta =  (datetime.now() + timedelta(hours=2)) - exitTimes[i]
+                timeDelta =  datetime.now() + timedelta(hours=2) - exitTimes[i]
                 minuteDelta = timeDelta.seconds // 60
                 
                 self.bar[i]["label"].config(text=minuteDelta)
